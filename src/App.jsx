54734b0e30481d6ccd4c218a98f4e33a3c22962a
DANISH_AHMED <Danish.Ahmed@studentambassadors.com>
2024-01-27 18:40:40 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Danish Ahmed",
+  title: "Ethical Hacker & Pentester",
+  email: "danishahmed2004505@gmail.com",
+  gitHub: "DanishAhmed2004",
+  instagram: "danii____khan",
+  linkedIn: "danish-ahmed-61927b265",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "DanishAhmed2004",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Blog website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Upcomming in process",
+    url: "Waiting...!",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Hacker and Pentester",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is my link of Tryhackme Profile",
+    url: "https://tryhackme.com/p/danishahmed20045",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Still in Process",
+    url: "Waiting...!",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Bug Hunting methodology",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Bug hunting Recon methodology by Jason Haddix.",
+    url: "https://www.youtube.com/watch?v=p4JgIu1mceI",
   },
 ];
 
